c26ae7a3305712b3a1e09f00fe71ca39a7a3c9b4
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-09-23 09:33:19 +0200

    add redux dev-tools

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import CommentsListContainer from './CommentsListContainer';
+import DevTools from './DevTools';
 
 const App = () => {
     return (
         <div className="App">
-            Tu będzie lista komentarzy!
+            <CommentsListContainer />
+            <DevTools />
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './reducer'
-const store = createStore(reducer);
+import { createStore, combineReducers } from 'redux';
+import reducer from './reducer';
+import {addComment} from './actions';
+import DevTools from './DevTools'
+
+const store = createStore(
+    reducer,
+    DevTools.instrument()
+);
 
 ReactDOM.render(
     <Provider store={store}>
